cdbfdceacd43c742a39633c8c1662324b0aa4486
slavawashere <slavawashere@ya.ru>
2018-08-17 15:42:01 +0300

    open and type json-file to console in utf-8

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,26 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    file = open(filepath, "r", encoding='utf-8')
+    print("opened")
+    data = file.read()
+    print("readed")
+    file.close()
+    print("closed")
+    return data
+    
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data,sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    if len (sys.argv) == 1:
+        print ("Error: expected path to json-file")
+    else:
+        file_name = sys.argv[1]
+        pretty_print_json(load_data(file_name))
+
